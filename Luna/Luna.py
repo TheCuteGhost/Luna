df8692abb449bdb4fd6b906b8fb16cf80f343e8c
ElCuteGhost <114844868+ElCuteGhost@users.noreply.github.com>
2022-10-03 18:44:22 -0500

    Update Luna.py
    Now you can play a song and it will take you to YouTube.

--- Luna/Luna.py
+++ Luna/Luna.py
@@ -6,23 +6,41 @@ listener = sr.Recognizer()
 engine = pyttsx3.init()
 
 voices = engine.getProperty('voices')
-engine.setProperty('voices', voices[0].id)
+engine.setProperty('voice', voices[0].id)
 
-def talk():
+def talk(text):
     engine.say(text)
     engine.runAndWait()
 
 def listen():
-    try:
-        with sr.Microphone() as source:
-            print("Luna esta escuchando...")
-            pc = listener.listen(source)
-            rec = listener.recognize_google(pc)
-            rec = rec.lower()
+    listener = sr.Recognizer()     
+    with sr.Microphone() as source:
+        print("Luna esta escuchando...")
+        listener.adjust_for_ambient_noise(source)
+        pc = listener.listen(source)
 
-            if name in rec:
-                rec = rec.remplace(name, '')
-    except:
-        pass
+    try:
+        rec = listener.recognize_google(pc, language="es")
+        rec = rec.lower()
+    except sr.UnknownValueError:
+        print("No te entendí, intenta de nuevo")
+        if name in rec:
+            rec = rec.replace(name, '')
     return rec
 
+def run_luna():
+    while True:
+        try:
+            rec = listen()
+        except UnboundLocalError:
+            print("No te entendí, intenta de nuevo")
+            continue     
+
+        if 'reproduce' in rec:
+            music = rec.replace('reproduce', '')
+            print("Reproduciendo " + music)
+            talk("Reproduciendo " + music)
+            pywhatkit.playonyt(music)
+
+if __name__ == '__main__':
+    run_luna()
